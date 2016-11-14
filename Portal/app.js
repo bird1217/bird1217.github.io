b733b119cf249880f6de7f64315a9fa3114828d4
ben.tseng <bird1217@gmail.com>
2016-11-14 14:44:33 +0800

    undo file

--- Portal/app.js
+++ Portal/app.js
@@ -1,4 +1,4 @@
-angular.module('ionicApp', ['ionic'])
+﻿angular.module('ionicApp', ['ionic'])
 
   .config(function ($stateProvider, $urlRouterProvider) {
 
@@ -9,12 +9,12 @@ angular.module('ionicApp', ['ionic'])
       controller: 'SignInCtrl'
     };
 
-     var tabs = {
-       name: 'tabs',
-       url: '/tab',
-       abstract: true,
-       templateUrl: 'templates/tabs.html'
-     };
+    var tabs = {
+      name: 'tabs',
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    };
 
     var tabHome = {
       name: 'tabs.home',
