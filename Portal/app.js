91daf9d1e2efc1cf71dd6e5dee5b40f14d7be5ab
ben.tseng <bird1217@gmail.com>
2016-11-14 13:56:46 +0800

    加入抽象的檔案

--- Portal/app.js
+++ Portal/app.js
@@ -1,55 +1,73 @@
-angular.module('ionicApp', ['ionic'])
+﻿angular.module('ionicApp', ['ionic'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  var settings = {
-	      name: 'signin',
-	      url: '/sign-in',	
-	      templateUrl: 'sign-in.html',
-	      controller: 'SignInCtrl'
-};
+    var settings = {
+      name: 'signin',
+      url: '/sign-in',
+      templateUrl: 'sign-in.html',
+      controller: 'SignInCtrl'
+    };
 
+    var tabs = {
+      name: 'tabs',
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    };
 
-$stateProvider.state(settings);
-   $urlRouterProvider.otherwise('/sign-in');
-})
-
-.controller('SignInCtrl', function($scope, $state) {
-  
-  $scope.signIn = function(user) {
-    console.log('Sign-In', user);
-    $state.go('tabs.home');
-  };
-  
-  			$scope.activeTemplate = 'content.html';
-
-})
-
-.controller('HomeTabCtrl', function($scope) {
-  console.log('HomeTabCtrl');
-
-
-$scope.clientSideList = [
-    { text: "Backbone", value: "bb" },
-    { text: "工號", value: "ng" },
-    { text: "中文姓名", value: "em" },
-    { text: "英文姓名", value: "ko" },
-    { text: "分機號碼", value: "bb2" }
-  ];
-
-  $scope.serverSideList = [
-    { text: "Go", value: "go" },
-    { text: "Python", value: "py" },
-    { text: "Ruby", value: "rb" },
-    { text: "Java", value: "jv" }
-  ];
-  
-  $scope.data = {
-    clientSide: 'bb'
-  };
-  
-  
+    var tabHome = {
+      name: 'tabs.home',
+      url: '/home',
+      views: {
+        'home-tab': {
+          templateUrl: 'templates/home.html',
+          controller: 'HomeTabCtrl'
+        }
+      }
+    };
 
 
-  
-});
+
+    $stateProvider
+      .state(settings)
+      .state(tabs)
+      .state(tabHome);
+
+
+    $urlRouterProvider.otherwise('/sign-in');
+  })
+
+  .controller('SignInCtrl', function ($scope, $state) {
+
+    $scope.signIn = function (user) {
+      console.log('Sign-In', user);
+      $state.go('tabs.home');
+    };
+
+    $scope.activeTemplate = 'content.html';
+
+  })
+
+  .controller('HomeTabCtrl', function ($scope) {
+    console.log('HomeTabCtrl');
+
+    $scope.clientSideList = [
+      { text: "Backbone", value: "bb" },
+      { text: "工號", value: "ng" },
+      { text: "中文姓名", value: "em" },
+      { text: "英文姓名", value: "ko" },
+      { text: "分機號碼", value: "bb2" }
+    ];
+
+    $scope.serverSideList = [
+      { text: "Go", value: "go" },
+      { text: "Python", value: "py" },
+      { text: "Ruby", value: "rb" },
+      { text: "Java", value: "jv" }
+    ];
+
+    $scope.data = {
+      clientSide: 'bb'
+    };
+  });
