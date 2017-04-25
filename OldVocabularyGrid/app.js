15f3151d7d531734b53653ba5f89645ebed547f7
ben.tseng <bird1217@gmail.com>
2017-04-25 15:16:45 +0800

    加入master detail畫面呈現

--- OldVocabularyGrid/app.js
+++ OldVocabularyGrid/app.js
@@ -9,20 +9,31 @@
       templateUrl: "templates/tabs.html"
     })
     .state('tabs.home', {
-      url: "/home",
-      views: {
-        'home-tab': {
-          //templateUrl: "templates/home.html"
-		  templateUrl: function (stateParams){			
-			return "templates/home.html";
-		  },		  
-          controller: 'HomeTabCtrl'
-        }
-      }
+      url: '/home',
+      templateUrl: 'templates/home.html',
+      controller: 'HomeTabCtrl'
+    })
+	.state('tabs.home2', {
+      url: '/home2/:parentId',
+      templateUrl: 'templates/home2.html',
+      controller: 'DetailTabCtrl',
+	  resolve: {		  
+		  
+	  }
     });
 	
     
-	
+	//.state('tabs.home', {
+    //  url: "/home",
+    //  views: {
+    //    'home-tab': {
+	//	  templateUrl: function (stateParams){			
+	//		return "templates/home.html";
+	//	  },		  
+    //      controller: 'HomeTabCtrl'
+    //    }
+    //  }
+    //})
     
 
    $urlRouterProvider.otherwise("/tab/home");
