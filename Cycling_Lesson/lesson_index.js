85267d257ccbf01d62a9192e05682e855a7fafa8
bird1217 <bird1217@gmail.com>
2023-09-06 15:32:21 +0800

    Add files via upload

---
+++ Cycling_Lesson/lesson_index.js
@@ -0,0 +1,155 @@
+var trainingArray = [{"Name":"zone2_2hr","TrainingData":[{"ftp":0.75,"time":"120m","color":"#3DB33F"}]},{"Name":"zone3+zone2_1HR","TrainingData":[{"ftp":0.73,"time":"5m","color":"#3DB33F"},{"ftp":0.9,"time":"5m","color":"#FCD549"},{"ftp":0.73,"time":"5m","color":"#3DB33F"},{"ftp":0.9,"time":"5m","color":"#FCD549"},{"ftp":0.73,"time":"5m","color":"#3DB33F"},{"ftp":0.9,"time":"5m","color":"#FCD549"},{"ftp":0.73,"time":"5m","color":"#3DB33F"},{"ftp":0.9,"time":"5m","color":"#FCD549"},{"ftp":0.75,"time":"5m","color":"#3DB33F"},{"ftp":1.0,"time":"10m","color":"#FC9C49"},{"ftp":0.7,"time":"10m","color":"#3DB33F"}]},{"Name":"zone3_3hr","TrainingData":[{"ftp":0.85,"time":"60m","color":"#FCD549"},{"ftp":0.82,"time":"60m","color":"#FCD549"},{"ftp":0.78,"time":"60m","color":"#FCD549"}]},{"Name":"zone4","TrainingData":[{"ftp":0.55,"time":"5m","color":"#3DB39F"},{"ftp":1.03,"time":"4m","color":"#FC9C49"},{"ftp":0.45,"time":"2m","color":"#3DB39F"},{"ftp":0.97,"time":"20m","color":"#FC9C49"},{"ftp":0.35,"time":"10m","color":"#3DB39F"},{"ftp":0.97,"time":"20m","color":"#FC9C49"},{"ftp":0.35,"time":"10m","color":"#3DB39F"},{"ftp":0.97,"time":"20m","color":"#FC9C49"},{"ftp":0.35,"time":"10m","color":"#3DB39F"}]},{"Name":"zone4_v2_15min","TrainingData":[{"ftp":0.6,"time":"3m","color":"#3DB33F"},{"ftp":0.7,"time":"3m","color":"#3DB33F"},{"ftp":0.8,"time":"3m","color":"#FCD549"},{"ftp":0.9,"time":"3m","color":"#FCD549"},{"ftp":0.55,"time":"5m","color":"#3DB39F"},{"ftp":1.03,"time":"4m","color":"#FC9C49"},{"ftp":0.45,"time":"2m","color":"#3DB39F"},{"ftp":1.05,"time":"3m15s","color":"#FC9C49"},{"ftp":0.95,"time":"4m15s","color":"#FC9C49"},{"ftp":1.05,"time":"3m15s","color":"#FC9C49"},{"ftp":0.95,"time":"4m15s","color":"#FC9C49"},{"ftp":0.45,"time":"8m","color":"#3DB39F"},{"ftp":1.05,"time":"3m15s","color":"#FC9C49"},{"ftp":0.95,"time":"4m15s","color":"#FC9C49"},{"ftp":1.05,"time":"3m15s","color":"#FC9C49"},{"ftp":0.95,"time":"4m15s","color":"#FC9C49"},{"ftp":0.45,"time":"8m","color":"#3DB39F"},{"ftp":1.05,"time":"3m15s","color":"#FC9C49"},{"ftp":0.95,"time":"4m15s","color":"#FC9C49"},{"ftp":1.05,"time":"3m15s","color":"#FC9C49"},{"ftp":0.95,"time":"4m15s","color":"#FC9C49"},{"ftp":0.45,"time":"8m","color":"#3DB39F"}]},{"Name":"zone4_v2_20min","TrainingData":[{"ftp":0.55,"time":"5m","color":"#3DB39F"},{"ftp":1.03,"time":"4m","color":"#FC9C49"},{"ftp":0.45,"time":"2m","color":"#3DB39F"},{"ftp":1.05,"time":"5m","color":"#FC9C49"},{"ftp":0.92,"time":"5m","color":"#FC9C49"},{"ftp":1.05,"time":"5m","color":"#FC9C49"},{"ftp":0.92,"time":"5m","color":"#FC9C49"},{"ftp":0.45,"time":"10m","color":"#3DB39F"},{"ftp":1.05,"time":"5m","color":"#FC9C49"},{"ftp":0.92,"time":"5m","color":"#FC9C49"},{"ftp":1.05,"time":"5m","color":"#FC9C49"},{"ftp":0.92,"time":"5m","color":"#FC9C49"},{"ftp":0.45,"time":"10m","color":"#3DB39F"},{"ftp":1.05,"time":"5m","color":"#FC9C49"},{"ftp":0.92,"time":"5m","color":"#FC9C49"},{"ftp":1.05,"time":"5m","color":"#FC9C49"},{"ftp":0.92,"time":"5m","color":"#FC9C49"},{"ftp":0.45,"time":"10m","color":"#3DB39F"}]},{"Name":"zone5","TrainingData":[{"ftp":0.6,"time":"3m","color":"#3DB33F"},{"ftp":0.7,"time":"3m","color":"#3DB33F"},{"ftp":0.8,"time":"3m","color":"#FCD549"},{"ftp":0.55,"time":"2m","color":"#3DB39F"},{"ftp":1.05,"time":"5m","color":"#FC9C49"},{"ftp":0.45,"time":"5m","color":"#3DB39F"},{"ftp":1.1,"time":"5m","color":"#E34074"},{"ftp":0.45,"time":"5m","color":"#3DB39F"},{"ftp":1.1,"time":"5m","color":"#E34074"},{"ftp":0.45,"time":"5m","color":"#3DB39F"},{"ftp":1.1,"time":"5m","color":"#E34074"},{"ftp":0.45,"time":"5m","color":"#3DB39F"},{"ftp":1.1,"time":"5m","color":"#E34074"},{"ftp":0.45,"time":"5m","color":"#3DB39F"},{"ftp":1.1,"time":"5m","color":"#E34074"},{"ftp":0.45,"time":"5m","color":"#3DB39F"},{"ftp":0.8,"time":"30m","color":"#FCD549"}]}];
+
+var weight;
+$(document).ready(function () {
+			
+	var result =queryTrainingData('zone2_2hr');
+		
+	console.log(result);
+	
+    let url_string = window.location;
+    let url = new URL(url_string);
+
+    weight = url.searchParams.get("w");
+    if (weight == null) {
+        weight = 68;
+    }
+
+    var targetFTP = getDefaultFTP();
+
+    var ftpWeight = parseFloat(parseInt(targetFTP) / parseInt(weight)).toFixed(2);
+
+
+
+    $('#ftpWeight').text(ftpWeight);
+
+    initJQTable(parseFloat(targetFTP));	
+});
+
+function btnClick() {
+    var targetFTP = $('#ftpValue').val();
+
+    var targetWeight = parseInt($('#weightValue').val());
+
+    if (parseInt(targetWeight)) {
+        weight = targetWeight;
+    }
+	if (isNaN(parseInt(targetFTP)))
+		targetFTP = getDefaultFTP();
+	
+    
+	$('#example tbody').empty();
+
+	$('#ftpZoneTable tbody').empty();
+
+	var ftpWeight = parseFloat(parseInt(targetFTP) / parseInt(weight)).toFixed(2);
+	$('#ftpWeight').text(ftpWeight);
+	
+	var lessonName = $('#Lesson').val();
+	initJQTable(+targetFTP,lessonName);    
+}
+
+function initJQTable(targetFTP,name) {    
+	var traingingData =[];
+	if(name==undefined || name=='')
+	{
+		traingingData= queryTrainingData('zone2_2hr')[0]["TrainingData"];
+	}
+	else		
+	{
+		traingingData= queryTrainingData(name)[0]["TrainingData"];
+	}
+		
+	
+	$('#traingTable').DataTable({paging: false, ordering: false, info: false, bFilter: false, bInfo: false, destroy: true, dom: 'Bfrtip'});
+    $('#ftpZoneTable').DataTable({paging: false, ordering: false, info: false, bFilter: false, bInfo: false, destroy: true, dom: 'Bfrtip'});
+	
+	 $.each(traingingData, function (key, value) {
+        var ftpValue = targetFTP;
+        var cell0 = Math.floor(value["ftp"]*targetFTP);
+                
+        var background = value["color"];
+
+		var cell1 = value["time"];
+
+        student += '<tr>';
+
+        var colorClass = '';
+
+        student += '<td colspan="2" style="background-color:' + background + ';"">' + cell0 +'   @'+ cell1 +'</td>';
+		
+                
+        student += '</tr>';
+    });
+    $('#traingTable').append(student);   
+	
+	
+    $(".dataTables_empty").empty();
+    $('.odd').hide();
+
+   
+
+    var ftpRange = [
+          { "Zone": 1, "From": 0, "To": 0.55, "Color": "#3DB39F" }
+        , { "Zone": 2, "From": 0.56, "To": 0.75, "Color": "#3DB33F" }
+        , { "Zone": 3, "From": 0.76, "To": 0.9, "Color": "#FCD549" }
+        , { "Zone": 4, "From": 0.91, "To": 1.05, "Color": "#FC9C49" }
+        , { "Zone": 5, "From": 1.06, "To": 1.2, "Color": "#E34074" }
+        , { "Zone": 6, "From": 1.21, "To": 1.5, "Color": "#8963D8" }];
+
+
+    var student = '';
+    $('#weight').text('Weight:' + weight);
+    $('#ftp').text('FTP:' + targetFTP);
+    $('#ftpZone').text('FTP:' + targetFTP);
+
+    $.each(ftpRange, function (key, value) {
+        var ftpValue = targetFTP;
+        var cell0 = Math.floor(value["From"] * 100) + '%';
+        var cell1 = Math.floor(value["To"] * 100) + '%';
+        var cell2 = value["Zone"];
+        var background = value["Color"];
+
+        var ftpFrom = ftpValue * value["From"];
+        var ftpTo = ftpValue * value["To"];
+
+        student += '<tr>';
+
+        var colorClass = '';
+
+        student += '<td class="cellRight_nonFTP" style="background-color:' + background + ';"">' + cell0 + '</td>';
+        student += '<td class="cellRight_nonFTP" style="background-color:' + background + ';"">' + cell1 + '</td>';
+        student += '<td class="cellRight_nonFTP" style="background-color:' + background + ';"">' + cell2 + '</td>';
+        student += '<td class="cellRight_nonFTP" style="background-color:' + background + ';"">' + Math.floor(ftpFrom) + '</td>';
+        student += '<td class="cellRight_nonFTP" style="background-color:' + background + ';"">' + Math.floor(ftpTo) + '</td>';
+
+        var ftpDivideWeight = (Math.floor(ftpFrom)/weight).toFixed(1) +"~" + (Math.floor(ftpTo)/weight).toFixed(1);
+        student += '<td class="cellRight_nonFTP">' + ftpDivideWeight + '</td>';
+
+        student += '</tr>';
+    });
+    $('#ftpZoneTable').append(student);    
+		
+}
+
+
+function queryTrainingData(name)
+{
+	var result =$.grep(trainingArray, function (h) {
+	  return h.Name == name;
+	});
+	
+	return result;
+}
+
+function getDefaultFTP()
+{
+	let url_string = window.location;
+    let url = new URL(url_string);
+	
+	var targetFTP = url.searchParams.get("ftp");
+    if (targetFTP == null) {
+        targetFTP = 265;
+    }
+	return targetFTP;
+}
